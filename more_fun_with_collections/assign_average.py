870403b04136bff1715b688815bbd8fe460a1fe3
packe <cspfander@dmacc.edu>
2019-10-14 15:27:39 -0500

    Re-did function after multiple testing, simplified with exception handling and handling if the key given is out of range

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -10,12 +10,12 @@ The purpose of this program is to implement a switch/case using a dictionary and
 def switch_average(key):
     switch_dictionary = {"A": 90, "B": 80, "C": 70, "D": 60, "F": 50}
     try:
-        for a_key in switch_dictionary:
-            if key == a_key:
-                return switch_dictionary[a_key]
+        return switch_dictionary.get(key, "Invalid key, try again!")
     except ValueError:
+        print("Invalid key, try again!")
+    except KeyError:
         print("Invalid key, try again!")
 
 
 if __name__ == '__main__':
-    print(switch_average("A"))
+    print(switch_average("G"))
