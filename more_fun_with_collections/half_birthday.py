0655cc8f3b13303c73df0f451a3feeed0a0b4ab0
packe <cspfander@dmacc.edu>
2019-10-14 15:52:35 -0500

    Added Function Body - All Tests Pass

--- more_fun_with_collections/half_birthday.py
+++ more_fun_with_collections/half_birthday.py
@@ -5,11 +5,13 @@ Last date modified: 10/14/2019
 
 The purpose of this program is to write a function to compute a half birthday
 """
+from datetime import timedelta, date
 
 
-def half_birthday():
-    pass
+def a_half_birthday(recent_birthday):
+    return recent_birthday + timedelta(days=182.5)
 
 
 if __name__ == '__main__':
-    half_birthday()
+    my_recent_birthday = date(2019, 4, 24)
+    print(a_half_birthday(my_recent_birthday))
--- test/test_half_birthday.py
+++ test/test_half_birthday.py
@@ -1,13 +1,13 @@
 import unittest
-from datetime import datetime
-from more_fun_with_collections import half_birthday
+from datetime import date
+from more_fun_with_collections.half_birthday import a_half_birthday
 
 
 class MyTestCase(unittest.TestCase):
     def test_half_birthday(self):
-        recent_birthday = datetime.datetime(2019, 4, 24)
-        expected_result = datetime.datetime(2019, 10, 23)
-        self.assertEqual(expected_result, half_birthday(recent_birthday))
+        recent_birthday = date(2019, 4, 24)
+        expected_result = date(2019, 10, 23)
+        self.assertEqual(expected_result, a_half_birthday(recent_birthday))
 
 
 if __name__ == '__main__':
