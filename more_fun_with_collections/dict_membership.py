458ef94f6433c755eefab105e174cd007edfee86
packe <cspfander@dmacc.edu>
2019-10-14 13:47:54 -0500

    Added function body and changed formatting some - All tests pass

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -8,9 +8,13 @@ if the element is in the dict.
 """
 
 
-def in_dict():
-    pass
+def in_dict(a_dict, a_value):
+    return a_value in a_dict
 
 
 if __name__ == '__main__':
-    in_dict()
+    my_dict = {"A": 90, "B": 80, "C": 70}
+    value = "A"
+    print(in_dict(my_dict, value))
+    value = "D"
+    print(in_dict(my_dict, value))
