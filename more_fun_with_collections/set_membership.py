d69317dbe70bdb32edcb310f9e5368c6ab65f2db
packe <cspfander@dmacc.edu>
2019-10-14 13:30:15 -0500

    Function Added - All Tests Pass

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -8,9 +8,16 @@ if the element is in the set.
 """
 
 
-def in_set():
-    pass
+def in_set(a_set):
+    my_set = set(a_set)
+    if a_set == my_set:
+        return True
+    if a_set != my_set:
+        return False
 
 
 if __name__ == '__main__':
-    in_set()
+    b_set = {1, 2, 3}
+    b_list = [1, 2, 3]
+    print(in_set(b_set))    # Example of a set being passed
+    print(in_set(b_list))   # Example of a non set being passed
